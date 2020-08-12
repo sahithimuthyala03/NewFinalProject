5524ce1764312bd02d4ad7b07eadd0cdbf1fd7d3
sahithimuthyala03 <69254657+sahithimuthyala03@users.noreply.github.com>
2020-08-12 12:08:22 -0400

    Add files via upload

---
+++ finalProject/PeopleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PeopleViewController.swift
+//  finalProject
+//
+//  Created by Sahithi Muthyala on 8/12/20.
+//  Copyright © 2020 Sahithi Muthyala. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ finalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finalProject
+//
+//  Created by Sahithi Muthyala on 8/11/20.
+//  Copyright © 2020 Sahithi Muthyala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
